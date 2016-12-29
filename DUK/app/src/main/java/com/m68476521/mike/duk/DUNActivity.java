9a39a6f13e94ceb240bf332f13c6a804fad5419d
Mike <m68476521.1@gmail.com>
2016-12-29 14:01:29 -0800

    feat: Testing commit

--- DUK/app/src/main/java/com/m68476521/mike/duk/DUNActivity.java
+++ DUK/app/src/main/java/com/m68476521/mike/duk/DUNActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DUNActivity extends AppCompatActivity {
+    //Testing commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
