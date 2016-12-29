1c72879277892525719f3d8bb08c6f559eea6434
Mike <m68476521.1@gmail.com>
2016-12-28 22:21:58 -0800

    Add initial project

---
+++ DUK/app/src/main/java/com/m68476521/mike/duk/DUNActivity.java
@@ -0,0 +1,13 @@
+package com.m68476521.mike.duk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DUNActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dun);
+    }
+}
